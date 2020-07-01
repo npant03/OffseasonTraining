3af2a5adc662bcb0b222ee8f97393fd69d903f87
Neha Pant <npant03@gmail.com>
2020-06-30 18:15:50 -0700

    complains, button binding instantiates IntakeSub, has the CTRE_PhoenixCCI error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,9 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.team7419.PaddedXbox;
 
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.CanIds;
 import frc.robot.subsystems.intake.*;
 
@@ -21,11 +23,14 @@ public class RobotContainer {
   // The commands that run on those subsystems are defined here
   private final RunIntake runIntake = new RunIntake(intake, .5);
 
+  private PaddedXbox xbox;
+
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
-  public RobotContainer() {
+  public RobotContainer(PaddedXbox xbox) {
     // Configure the button bindings
+    this.xbox = xbox;
     configureButtonBindings();
   }
 
@@ -33,6 +38,8 @@ public class RobotContainer {
    * Use this method to define your button->command mappings. 
    * We're going to teach you how to use this later.
    */
-  private void configureButtonBindings() {
+  public void configureButtonBindings() {
+    new JoystickButton(xbox, PaddedXbox.F310Map.kGamepadButtonX.value)
+    .whenPressed(new RunIntake(intake, .5));
   }
 }
---
+++ src/test/java/frc/robot/subsystems/Week4ButtonBindingTest.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import static org.mockito.Mockito.mock;
+
+import com.team7419.PaddedXbox;
+
+import org.junit.Test;
+
+import frc.robot.RobotContainer;
+
+public class Week4ButtonBindingTest {
+
+    PaddedXbox xbox = mock(PaddedXbox.class);
+    RobotContainer robotContainer = new RobotContainer(xbox);
+
+    @Test
+    public void runXbox(){
+        robotContainer.configureButtonBindings();
+        /**
+         * once we fix padded xbox, in order to make a button binding, you'll have to
+         * call a method w return type JoystickButton on the PaddedXbox object
+         * it'll probably look like 
+         * public JoystickButton getX(){
+         *      return new JoystickButton(this, PaddedXbox.F310Map.kGamepadButtonX.value)
+         * }
+         * so this test can just do this
+         * verify(xbox).getX().whenPressed(new RunIntake(intake, .5)) or whatever
+         */
+
+    }
+    
+}
