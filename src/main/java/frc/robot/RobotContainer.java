146e69cc2535bc27e95bc9197e8445702b3273dd
Neha Pant <npant03@gmail.com>
2020-07-09 11:06:14 -0700

    remove answers from week 4 compiles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,5 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.team7419.PaddedXbox;
-
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.CanIds;
-import frc.robot.subsystems.intake.*;
-
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -15,17 +8,12 @@ import frc.robot.subsystems.intake.*;
  */
 public class RobotContainer {
 
-  private Factory factory;
-  private IntakeSub intake;
-  private PaddedXbox joystick;
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
-  public RobotContainer(Factory factory) {
-    this.factory = factory;
-    intake = factory.getIntakeSub();
-    joystick = factory.getPaddedXbox();
+  public RobotContainer() {
+    // Configure the button bindings
     configureButtonBindings();
   }
 
@@ -33,8 +21,6 @@ public class RobotContainer {
    * Use this method to define your button->command mappings. 
    * We're going to teach you how to use this later.
    */
-  public void configureButtonBindings() {
-    joystick.getA().whenPressed(factory.getRunIntake(.5));  
+  private void configureButtonBindings() {
   }
-  
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSub.java
@@ -1,32 +1,25 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.IMotorController;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.team7419.Initers;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
- * actual ball intake, as in like the rollers
+ * For your first code assignment, you're going to be writing a subsystem 
+ * for the intake on our 2020 robot. There's a tutorial on Notion that's going 
+ * to step you through 
  */
-public class IntakeSub extends SubsystemBase{
-    
-    private VictorSPX victor;
 
-    public IntakeSub(VictorSPX victor){
-        this.victor = victor;
-        Initers.initVictors(victor);
-        victor.setInverted(false);
-    }
+ //comment 
+ 
+public class IntakeSub extends SubsystemBase {
+  /**
+   * Creates a new IntakeSub.
+   */
+  public IntakeSub() {
 
-    @Override
-    public void periodic(){}
+  }
 
-    public void setPower(double power){
-        victor.set(ControlMode.PercentOutput, power);
-    }
-
-    // completely trivial, only for a unit test
-    public boolean getInverted(){return victor.getInverted();}
-
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
