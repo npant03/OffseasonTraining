6f3452d09a865ef1e27dbf0bdc5a299d6440cdd6
Neha Pant <npant03@gmail.com>
2020-07-23 14:00:26 -0700

    move drivebase and update imports

--- src/main/java/frc/robot/Factory.java
+++ src/main/java/frc/robot/Factory.java
@@ -2,11 +2,11 @@ package frc.robot;
 
 import com.team7419.PaddedXbox;
 
-import frc.robot.subsystems.intake.DriveBaseSub;
+import frc.robot.subsystems.drivebase.DriveBaseSub;
 import frc.robot.subsystems.intake.IntakeSub;
 import frc.robot.subsystems.intake.RunIntake;
 import frc.robot.subsystems.intake.RunIntakeWithJoystick;
-import frc.robot.subsystems.intake.TankDrive;
+import frc.robot.subsystems.drivebase.TankDrive;
 
 public interface Factory {
     
