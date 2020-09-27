496524e7827fde2483826df64c567a3caf7e8063
Henry M <henrymazaika@gmail.com>
2020-09-27 15:08:06 -0700

    added empty command group (sequential)

---
+++ src/main/java/frc/robot/snippits/StraightThenTurn.java
@@ -0,0 +1,16 @@
+package frc.robot.snippits;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class StraightThenTurn extends SequentialCommandGroup {
+
+
+  /**
+   * Creates a new StraightThenTurn.
+   */
+  public StraightThenTurn() {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super();
+  }
+}
