d14ecf807526b236546d1d0caeb11b5e6c3bcc1c
Neha Pant <npant03@gmail.com>
2020-10-10 14:27:10 -0700

    fix week 6 and 7

--- src/test/java/frc/robot/subsystems/Week6And7DriveTrainTest.java
+++ src/test/java/frc/robot/subsystems/Week6And7DriveTrainTest.java
@@ -1,222 +1,145 @@
 package frc.robot.subsystems;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.team7419.PaddedXbox;
 
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.RobotContainer;
-import frc.robot.subsystems.drivebase.ArcadeDrive;
-import frc.robot.subsystems.drivebase.DriveBaseSub;
-import frc.robot.subsystems.drivebase.TankDrive;
+import frc.robot.*;
+import frc.robot.subsystems.drivebase.*;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class Week6And7DriveTrainTest {
+public class Week6and7DriveTrainTest {
 
-    // DO NOT PUT ROBOTCONTAINER UP HERE.
     SimFactory simFactory = new SimFactory();
     PaddedXbox joystick = simFactory.getPaddedXbox();
-    DriveBaseSub driveBaseSub = simFactory.getDriveBaseSub();
-    TalonFX leftFront = driveBaseSub.getLeftMast();
-    TalonFX leftBack = driveBaseSub.getLeftFollow();
-    TalonFX rightFront = driveBaseSub.getRightMast();
-    TalonFX rightBack = driveBaseSub.getRightFollow();
+    DriveBaseSub driveBase = simFactory.getDriveBaseSub();
+    TalonFX leftMast = driveBase.getLeftMast();
+    TalonFX leftFollow = driveBase.getLeftFollow();
+    TalonFX rightMast = driveBase.getRightMast();
+    TalonFX rightFollow = driveBase.getRightFollow();
+    boolean following = false;
+    boolean leftFollowing = false;
+    boolean rightFollowing = false;
+        
     JoystickButton mockButton = mock(JoystickButton.class);
     TankDrive tankDrive = simFactory.getTankDrive(joystick);
     ArcadeDrive arcadeDrive = simFactory.getArcadeDrive(joystick);
 
-    /**
-     * Checks if RobotContainer's setDefaultCommands method sets a default command to DriveBaseSub
-     */
     @Test
-    public void driveBaseSubHasDefaultCommandTest() {
+    public void checkDefaultCommand(){
         when(joystick.getA()).thenReturn(mockButton);
         RobotContainer robotContainer = new RobotContainer(simFactory);
         robotContainer.setDefaultCommands();
-        assertNotEquals(null, driveBaseSub.getDefaultCommand());
+        Class defaultClass = driveBase.getDefaultCommand().getClass();
+        assertEquals(defaultClass == TankDrive.class | defaultClass == ArcadeDrive.class, true);
     }
 
-    /**
-     * Checks that the drive base's default command is TankDrive OR ArcadeDrive
-     */
     @Test
-    public void aDefaultCommandTest() {
-        try{
-        when(joystick.getA()).thenReturn(mockButton);
-        RobotContainer robotContainer = new RobotContainer(simFactory);
-        robotContainer.setDefaultCommands();
-        assertEquals(simFactory.getTankDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        System.out.println("DriveBaseSub's default command is TankDrive!");
-    } catch(AssertionError e){
-        when(joystick.getA()).thenReturn(mockButton);
-        RobotContainer robotContainer = new RobotContainer(simFactory);
-        robotContainer.setDefaultCommands();
-        assertEquals(simFactory.getArcadeDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        System.out.println("DriveBaseSub's default command is ArcadeDrive!");
+    public void checkIfFollowing(){
+        doAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+              leftFollowing = true;
+              return null;
+            }
+        }).when(leftFollow).follow(leftMast);
+
+        doAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+              rightFollowing = true;
+              return null;
+            }
+        }).when(rightFollow).follow(rightMast);
+
+        DriveBaseSub dummyDriveBase = new DriveBaseSub(leftFollow, leftMast, rightFollow, rightMast);
+        if(leftFollowing & rightFollowing){following = true;}
+    }
+
+    @Test
+    public void checkTankDriveControls(){
+        configureFollowers();
+
+        // check both sides are independent
+        setJoystickValues(0, .75, 0, .5);
+        tankDrive.execute();
+        verifyLeftRightSpeeds(0.75, 0.5);
+
+        // check setting negatives works
+        setJoystickValues(0, -.5, 0, -.3);
+        tankDrive.execute();
+        verifyLeftRightSpeeds(-0.5, -.3);
+
+        // check setting random things doesnt move
+        setJoystickValues(1, 0, 1, 0);
+        tankDrive.execute();
+        verifyLeftRightSpeeds(0, 0);
+    }
+
+    @Test
+    public void checkTankEndMethod(){
+        configureFollowers();
+        tankDrive.end(true);
+        verifyLeftRightSpeeds(0.0, 0.0);
+    }
+
+    @Test
+    public void checkArcadeDriveControls(){
+        configureFollowers();
+        double kStraight = PowerConstants.DriveBaseStraight.val;
+        double kTurn = PowerConstants.DriveBaseTurn.val;
+
+        // check straight
+        setJoystickValues(0, .2, 0, 0);
+        arcadeDrive.execute();
+        verifyLeftRightSpeeds(kStraight * 0.2, kStraight * 0.2);
+
+        // check left turn
+        setJoystickValues(0, 0, -.5, 0);
+        arcadeDrive.execute();
+        verifyLeftRightSpeeds(-kTurn * 0.5, kTurn * 0.5);
+
+        // check right turn
+        setJoystickValues(0, 0, .9, 0);
+        arcadeDrive.execute();
+        verifyLeftRightSpeeds(kTurn * 0.9, -kTurn * 0.9);
+
+        // check straight and right together
+        setJoystickValues(0, 0.7, 0.4, 0);
+        arcadeDrive.execute();
+        verifyLeftRightSpeeds(kStraight * 0.7 + kTurn * .4, kStraight * 0.7 - kTurn * .4);
+    }
+
+    @Test
+    public void checkArcadeEndMethod(){
+        configureFollowers();
+        arcadeDrive.end(true);
+        verifyLeftRightSpeeds(0, 0);
+    }
+
+    private void verifyLeftRightSpeeds(double left, double right){
+        verify(leftMast).set(ControlMode.PercentOutput, left);
+        verify(leftFollow).set(ControlMode.PercentOutput, left);
+        verify(rightMast).set(ControlMode.PercentOutput, right);
+        verify(rightFollow).set(ControlMode.PercentOutput, right);
+    }
+
+    private void setJoystickValues(double leftX, double leftY, double rightX, double rightY){
+        when(joystick.getLeftX()).thenReturn(leftX);
+        when(joystick.getLeftY()).thenReturn(leftY);
+        when(joystick.getRightX()).thenReturn(rightX);
+        when(joystick.getRightY()).thenReturn(rightY);
+    }
+
+    private void configureFollowers(){
+        if(following){
+            rightFollow = rightMast;
+            leftFollow = leftMast;
         }
     }
-
-    /**
-     * Checks that the left joystick correlates to drivetrain speed
-     */
-    @Test
-    public void driveBaseIsControlledWithLeftJoystickTest() {
-        // try{
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getTankDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getLeftY()).thenReturn(.75);
-        // tankDrive.execute();
-        // verify(leftFront).set(ControlMode.PercentOutput, .75);
-        // verify(rightFront).set(ControlMode.PercentOutput, 0.0);
-        //     try {
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        //     } catch (AssertionError e) {
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, .75);
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, 0);
-        //     }
-        // } catch(AssertionError e){
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getArcadeDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getLeftY()).thenReturn(.75);
-        // when(joystick.getRightX()).thenReturn(0.0);
-        // arcadeDrive.execute();
-        // verify(leftFront).set(ControlMode.PercentOutput, .375);
-        // verify(rightFront).set(ControlMode.PercentOutput, .375);
-        // try {
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        // } catch (AssertionError f) {
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, .375);
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, .375);;
-        //     }
-        // }
-    }
-
-    /**
-     * Checks that the right joystick correlates to drivetrain speed
-     */
-    @Test
-    public void driveBaseRightIsControlledWithRightJoystickTest(){   
-        // try {
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getTankDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getRightY()).thenReturn(.75);
-        // tankDrive.execute();
-        // verify(rightFront).set(ControlMode.PercentOutput, .75);
-        // verify(leftFront).set(ControlMode.PercentOutput, 0);
-        //     try {
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     } catch (AssertionError e) {
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, .75);
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, 0);;
-        //     }
-        // } catch (AssertionError e){
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getArcadeDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getLeftY()).thenReturn(0.0);
-        // when(joystick.getRightX()).thenReturn(.9);
-        // arcadeDrive.execute();
-        // verify(leftFront).set(ControlMode.PercentOutput, .27);
-        // verify(rightFront).set(ControlMode.PercentOutput, -.27);
-        //     try {
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        //     } catch (AssertionError f) {
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, .27);
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, -.27);;
-        //     }
-        // }
-    }
-    
-    /**
-     * Checks that moving the left and right joystick will move the drivetrain correctly
-     */
-    @Test
-    public void theDriveWorks(){  
-        // try{
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getTankDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getLeftY()).thenReturn(.633);
-        // when(joystick.getRightY()).thenReturn(.112);
-        // tankDrive.execute();
-        // verify(leftFront).set(ControlMode.PercentOutput, .633);
-        // verify(rightBack).set(ControlMode.PercentOutput, .112);
-        // try {
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        // } catch (AssertionError e) {
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, .633);
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, .112);;
-        //     }
-        // } catch(AssertionError e){
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getArcadeDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // when(joystick.getLeftY()).thenReturn(.5);
-        // when(joystick.getRightX()).thenReturn(.5);
-        // arcadeDrive.execute();
-        // verify(leftFront).set(ControlMode.PercentOutput, .4);
-        // verify(rightFront).set(ControlMode.PercentOutput, .1);
-        // try {
-        //     verify(leftBack, atLeast(0)).follow(leftFront);
-        //     verify(rightBack, atLeast(0)).follow(rightFront);
-        // } catch (AssertionError f) {
-        //     verify(leftBack, atLeast(0)).set(ControlMode.PercentOutput, .4);
-        //     verify(rightBack, atLeast(0)).set(ControlMode.PercentOutput, .1);;
-        //     }
-        // }
-    }
-
-    /**
-     * Checks that when the command *ENDS* hint hint, all 4 drive motors stop.
-     */
-    @Test
-    public void turnsOffWhenCommandEndsTest(){
-        // try {
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getTankDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // tankDrive.end(false);
-        // verify(leftFront).set(ControlMode.PercentOutput, 0);
-        // verify(rightFront).set(ControlMode.PercentOutput, 0);
-        // verify(leftBack).set(ControlMode.PercentOutput, 0);
-        // verify(rightBack).set(ControlMode.PercentOutput, 0);
-        // } catch(AssertionError e){
-        //     when(joystick.getA()).thenReturn(mockButton);
-        //     RobotContainer robotContainer = new RobotContainer(simFactory);
-        //     robotContainer.setDefaultCommands();
-        //     assertEquals(simFactory.getArcadeDrive(joystick).getClass(), driveBaseSub.getDefaultCommand().getClass());
-        // arcadeDrive.end(false);
-        // verify(leftFront).set(ControlMode.PercentOutput, 0);
-        // verify(rightFront).set(ControlMode.PercentOutput, 0);
-        // verify(leftBack).set(ControlMode.PercentOutput, 0);
-        // verify(rightBack).set(ControlMode.PercentOutput, 0);
-        // }
-    }
-
 }
